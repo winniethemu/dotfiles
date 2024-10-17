64fbb280b890383dd8b0849194d07780ee79f1f0
winniethemu <emailyemu@gmail.com>
2024-10-16 18:23:07 -0700

    macOS setup includes autojump

--- setup/macos.zsh
+++ setup/macos.zsh
@@ -8,6 +8,7 @@ ZSH=$HOME/.zsh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew install tmux
+brew install autojump
 brew install reattach-to-user-namespace
 
 # Config
